edfe594bce86eae14a1ac19d3dd849fae963a7b4
Sergey Gordeychuk <ya.gordeichuk@gmail.com>
2026-05-19 18:00:56 +0300

    Fixed home work

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
 
-rootProject.name = "kt-get-century-from-year"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 
+rootProject.name = "kt-get-century-from-year"
